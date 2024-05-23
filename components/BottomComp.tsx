50e755152293ca467f1191ad565c835fc301512e
kapilaklecha <kapilaklecha@gmail.com>
2024-05-23 18:41:15 +0530

    make it responsive

--- components/BottomComp.tsx
+++ components/BottomComp.tsx
@@ -5,9 +5,9 @@ import Container from './Container'
 const BottomComp = () => {
   return (
     <Container>
-    <div className=' flex items-start py-12 px-6'>
-        <div className='w-[50%] px-6'><img src="/prefooter.png" alt="" /></div>
-        <div className=' px-12'>
+    <div className=' flex items-start py-12 px-6 max-lg:flex-col'>
+        <div className='w-[50%] px-6 max-lg:w-[100%] max-lg:mb-12 max-lg:px-0 '><img src="/prefooter.png" alt="" /></div>
+        <div className=' px-12 max-lg:px-0'>
     <div className=' flex  flex-col '>
         <h1 className=' text-[2rem] text-[#011e41]  font-extrabold break-words  mb-6'>If you're eager to start a new home renovation project, we're experienced, established, and ready to get to work.
         </h1>
