c63cb07fd0dbedae9d90e93bdd08825726dc0b44
kapilaklecha <kapilaklecha@gmail.com>
2024-06-28 20:34:58 +0530

    make custom cards dynamic

--- app/services/_components/CustomCard.tsx
+++ app/services/_components/CustomCard.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import IconPencilRuler from './svgs/IconPencilRuler'
 
-const CustomCard = () => {
+const CustomCard = ({desc, heading} : {desc : string, heading: string}) => {
   return (
      <div className=' p-[24px]'>
     <div className=" bg-white p-12 border border-[#e5e5e5] rounded-sm max-lg:p-2">
       <div className='w-[96px]'><IconPencilRuler/></div>
      
-    <h2 className="text-3xl font-bold my-6">Custom Design + Build</h2>
+    <h2 className="text-3xl font-bold my-6">{heading}</h2>
     <p className="mb-4">
-      From the beginning, we listen, brainstorm, and discuss ideas to bring out the full potential of spaces and areas that are important to you. This step informs and educates you on what project ideas are possible and what budget you will need to complete them.
+      {desc}
     </p>
   </div>
   </div>
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -20,10 +20,13 @@ const page = () => {
         <Container>
          
         <div className=' grid grid-cols-2 max-lg:grid-cols-1 p-12 mt-[-128px] max-lg:p-6'>
-          <CustomCard/>
-          <CustomCard/>
-          <CustomCard/>
-          <CustomCard/>
+          <CustomCard heading={`Custom design + build
+`} desc='From the beginning, we listen, brainstorm, and discuss ideas to bring out the full potential of spaces and areas that are important to you. This step informs and educates you on what project ideas are possible and what budget you will need to complete them.'/>
+          <CustomCard heading='Architecture + engineering' desc="From ideas to drawn plans, we present the project in printed form with the help of our architect. Once the final draft is representative of your goals, we move on to engineering. And if building permit applications are required, we'll take care of them for you.
+
+"/>
+          <CustomCard heading='Interior + exterior design' desc="This final stage is all about attention to detail. Our designer will walk you through finishing selections, color coordination, window design, flooring, trim, and much much more. It's easy to feel overwhelmed, but we're here to help make it stress free and fun."/>
+          <CustomCard heading='Project management' desc="Once you give the green light on all the planning and design decisions, we're ready to start building. We supply and operate all trades, materials and supplies, as well as site operations. Our goal is always to keep the project on time and on budget"/>
         </div>
         </Container>
     </div>
