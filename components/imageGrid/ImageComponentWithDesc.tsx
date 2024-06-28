ce17c38061771eec44a0750a6a15d0dd7a12d155
kapilaklecha <kapilaklecha@gmail.com>
2024-06-28 20:35:52 +0530

    change the border color

--- components/imageGrid/ImageComponentWithDesc.tsx
+++ components/imageGrid/ImageComponentWithDesc.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ImageComponentWithDesc = ({source, heading, desc} : { source : string, heading: string, desc : string}) => {
   return (
     <div className="p-4  mx-auto">
-    <div className="border border-[#e5e5e5] rounded-sm overflow-hidden shadow-sm">
+    <div className="border border-[#f4f4f4] rounded-sm overflow-hidden shadow-sm">
       <div className="h-64  w-full overflow-hidden max-lg:h-full">
         <img src={source} alt={heading} className="h-full w-full object-cover" />
       </div>
