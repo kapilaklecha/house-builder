b66a37589f5eeb92f2bd7ef9c4775dd81dad6521
kapilaklecha <kapilaklecha@gmail.com>
2024-06-28 20:35:30 +0530

    increase the height

--- components/imageGrid/ImageComp.tsx
+++ components/imageGrid/ImageComp.tsx
@@ -6,7 +6,7 @@ const ImageComp = ({source, text } : {source: string, text : string}) => {
     <div className=' relative p-2'>
     <a href="">
       <figure className='group relative'>
-        <img src={source} alt="previous-project-image" className='w-full h-[269px]  max-lg:h-[100%] object-cover ' />
+        <img src={source} alt="previous-project-image" className='w-full h-[400px]  max-lg:h-[100%] object-cover ' />
         <figcaption>
           <div className='flex justify-between p-3 bg-white absolute bottom-0 left-0 right-0 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 max-lg:opacity-100'>
             <p className='font-bold'>{text}</p>
